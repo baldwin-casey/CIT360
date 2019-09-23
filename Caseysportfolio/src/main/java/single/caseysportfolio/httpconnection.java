3959562556919a57419123303033d306c53db4e3
Cryocannon <cbaldwin1992@gmail.com>
2019-09-23 08:34:01 -0700

    Fixed some code

--- Caseysportfolio/src/main/java/single/caseysportfolio/httpconnection.java
+++ Caseysportfolio/src/main/java/single/caseysportfolio/httpconnection.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package single.caseysportfolio;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 /**
@@ -11,12 +12,17 @@ import java.net.URL;
  * @author cbald
  */
 public class httpconnection {
-       public static void section() {
+       public static void section() throws Exception{
     System.out.println("HTTP!\n");
     String url = "http://www.google.com";
+    try {
     URL obj = new URL(url);
     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
   int status = con.getResponseCode();
   System.out.println("Google response code is " + status);
 }
+    catch (IOException e) {
+        
+    }
+    }
 }
