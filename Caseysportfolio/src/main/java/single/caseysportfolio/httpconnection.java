da33505d2c5ef8bd0f097747339d6274466fa1e3
Cryocannon <cbaldwin1992@gmail.com>
2019-09-23 08:26:57 -0700

    changed multiple files

--- Caseysportfolio/src/main/java/single/caseysportfolio/httpconnection.java
+++ Caseysportfolio/src/main/java/single/caseysportfolio/httpconnection.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package single.caseysportfolio;
-
+import java.net.HttpURLConnection;
+import java.net.URL;
 /**
  *
  * @author cbald
@@ -12,5 +13,10 @@ package single.caseysportfolio;
 public class httpconnection {
        public static void section() {
     System.out.println("HTTP!\n");
+    String url = "http://www.google.com";
+    URL obj = new URL(url);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+  int status = con.getResponseCode();
+  System.out.println("Google response code is " + status);
 }
 }
